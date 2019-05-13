9fdb6386f3ff3b5c69d34e1a951ef7103ba95be9
Bradley Axen <baxen@squareup.com>
2019-05-13 15:21:38 -0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+extras_require = {
+    'dev': ['pytest', 'flake8']
+}
+
+setup(
+    name='workflows',
+    version='0.1.0',
+    description='prototype',
+    long_description=readme,
+    author='',
+    author_email='',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    extras_require=extras_require,
+    zip_safe=False,
+    keywords='workflows',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
