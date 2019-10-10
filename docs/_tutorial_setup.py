0d6147725dd00b19ea68b140a7fb7676aff9cd0b
Janek Klawe <janek@squareup.com>
2019-10-10 16:02:08 -0400

    Improve wording in tutorials
    - Moved some text from the tutorials into code comments.
    - Made the ML Example a bit more standalone
    - Fixed some bugs in the _tutorial_setup file.  (For one thing, there
    were two of these files.)
    - Generally cleaned up the wording.

--- docs/_tutorial_setup.py
+++
@@ -1,6 +0,0 @@
-import sys
-from pathlib2 import Path
-
-project_path = str(Path('../..').resolve())
-if project_path not in sys.path:
-    sys.path.append(project_path)
--- docs/tutorials/_tutorial_setup.py
+++ docs/tutorials/_tutorial_setup.py
@@ -3,4 +3,4 @@ from pathlib2 import Path
 
 project_path = str(Path('../..').resolve())
 if project_path not in sys.path:
-    sys.path.append(project_path)
+    sys.path.insert(0, project_path)
