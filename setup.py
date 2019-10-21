f7ead1ef74d92bbd18214dedb4f17ce3a4d6ff40
Cathy Yeh <yeh@squareup.com>
2019-10-21 10:05:26 -0700

    Bump version to 0.5.3

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ requirements = [
 
 setup(
     name='bionic',
-    version='0.5.2',
+    version='0.5.3',
     description=(
         'A Python framework for building, running, and sharing data science '
         'workflows'),
