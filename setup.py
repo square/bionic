61d5d385e5cdcaa90c6837600b9a651280a905f5
Benjamin Clayman <bclayman@squareup.com>
2019-11-06 14:46:58 -0800

    Bump version: 0.5.6 → 0.5.7

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ requirements = [
 
 setup(
     name='bionic',
-    version='0.5.6',
+    version='0.5.7',
     description=(
         'A Python framework for building, running, and sharing data science '
         'workflows'),
