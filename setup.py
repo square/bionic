0d5e26ba1d58328d1c440d52cb8682a17d3b6a1a
Janek Klawe <janek@squareup.com>
2019-11-13 10:27:22 -0500

    Bump version: 0.6.0 → 0.6.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requirements = [
 
 setup(
     name='bionic',
-    version='0.6.0',
+    version='0.6.1',
     description=(
         'A Python framework for building, running, and sharing data science '
         'workflows'),
