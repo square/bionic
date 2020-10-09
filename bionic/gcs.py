5ea805e0c5a8f895614fdf650f4db4ae0a761337
Naman Jain <naman@squareup.com>
2020-10-09 11:56:46 -0400

    remove now unused GcsTool

--- bionic/gcs.py
+++ bionic/gcs.py
@@ -6,7 +6,6 @@ import subprocess
 import warnings
 
 from .deps.optdep import import_optional_dependency
-from .utils.urls import bucket_and_object_names_from_gs_url
 
 import logging
 
@@ -81,66 +80,6 @@ def copy_to_gcs(src, dst):
     client = get_gcs_client_without_warnings()
     blob = client.get_bucket(bucket).blob(path)
     blob.upload_from_filename(src)
-
-
-class GcsTool:
-    """
-    A helper object providing utility methods for accessing GCS.  Maintains
-    a GCS client, and a prefix defining a default namespace to read/write on.
-    """
-
-    _GS_URL_PREFIX = "gs://"
-
-    def __init__(self, url):
-        if url.endswith("/"):
-            url = url[:-1]
-        self.url = url
-        bucket_name, object_prefix = bucket_and_object_names_from_gs_url(url)
-
-        self._bucket_name = bucket_name
-        self._object_prefix = object_prefix
-        self._init_client()
-
-    def __getstate__(self):
-        # Copy the object's state from self.__dict__ which contains
-        # all our instance attributes. Always use the dict.copy()
-        # method to avoid modifying the original state.
-        state = self.__dict__.copy()
-        # Remove the unpicklable entries.
-        del state["_client"]
-        del state["_bucket"]
-        return state
-
-    def __setstate__(self, state):
-        # Restore instance attributes.
-        self.__dict__.update(state)
-        # Restore the client and bucket.
-        self._init_client()
-
-    def _init_client(self):
-        self._client = get_gcs_client_without_warnings()
-        self._bucket = self._client.get_bucket(self._bucket_name)
-
-    def blob_from_url(self, url):
-        object_name = self._validated_object_name_from_url(url)
-        return self._bucket.blob(object_name)
-
-    def url_from_object_name(self, object_name):
-        return self._GS_URL_PREFIX + self._bucket.name + "/" + object_name
-
-    def blobs_matching_url_prefix(self, url_prefix):
-        obj_prefix = self._validated_object_name_from_url(url_prefix)
-        return self._bucket.list_blobs(prefix=obj_prefix)
-
-    @staticmethod
-    def gsutil_cp(src_url, dst_url):
-        gsutil_cp(src_url, dst_url)
-
-    def _validated_object_name_from_url(self, url):
-        bucket_name, object_name = bucket_and_object_names_from_gs_url(url)
-        assert bucket_name == self._bucket.name
-        assert object_name.startswith(self._object_prefix)
-        return object_name
 
 
 def gsutil_cp(src_url, dst_url):
