0b8f18e2fc4fd3e3420eacb82a57e7b832f82ef4
Naman Jain <naman@squareup.com>
2020-10-09 22:46:22 -0400

    remove now unused gsutil fake code

--- tests/test_flow/fakes.py
+++ tests/test_flow/fakes.py
@@ -1,12 +1,9 @@
-import io
-import logging
-import os
 from contextlib import contextmanager
 from functools import partial
-from glob import glob
+import io
+import logging
 from pathlib import Path
 from unittest.mock import Mock
-
 from uuid import uuid4
 
 from bionic import gcs
@@ -167,123 +164,6 @@ class FakeGcsFs:
                 del self._files_by_url[file_url]
         else:
             del self._files_by_url[url]
-
-
-class FakeBlob:
-    def __init__(self, bucket, name, blobs):
-        assert not name.startswith("/")
-        self.bucket = bucket
-        self.name = name
-        self.blobs = blobs
-
-    def upload_from_string(self, data):
-        self.blobs[self._url] = data
-
-    def download_as_string(self):
-        return self.blobs[self._url]
-
-    def upload_from_filename(self, filename):
-        self.upload_from_string(open(filename, "r").read())
-
-    def download_to_filename(self, filename):
-        open(filename, "w").write(self.download_as_string())
-
-    def exists(self):
-        return self._url in self.blobs
-
-    def delete(self):
-        del self.blobs[self._url]
-
-    @property
-    def _url(self):
-        return f"gs://{self.bucket}/{self.name}"
-
-
-class FakeBucket:
-    def __init__(self, name, blobs):
-        self.name = name
-        self.blobs = blobs
-
-    def blob(self, blob_name):
-        return FakeBlob(self.name, blob_name, self.blobs)
-
-    def get_blob(self, blob_name):
-        return self.blob(blob_name)
-
-    def list_blobs(self, prefix):
-        assert not prefix.startswith("gs://")
-        return [
-            FakeBlob(self.name, k.replace(f"gs://{self.name}/", "", 1), self.blobs)
-            for (k, v) in self.blobs.items()
-            if k.startswith(f"gs://{self.name}/{prefix}")
-        ]
-
-
-class FakeGCS:
-    def __init__(self):
-        self.blobs = {}
-
-    def get_bucket(self, bucket):
-        return FakeBucket(bucket, self.blobs)
-
-    def wipe_path(self, url):
-        urls_to_remove = [k for k in self.blobs.keys() if k.startswith(url)]
-        for k in urls_to_remove:
-            del self.blobs[k]
-
-    # Allows the blob to be used like a file object.
-    # Used for mocking out blocks.filesystem.GCSNativeFileSystem.
-    @contextmanager
-    def open_blob(self, path, mode):
-        f = io.BytesIO(self.blobs.get(path, b""))
-        yield f
-        self.blobs[path] = f.getvalue()
-
-    def block_pickle(self, result, path):
-        self.blobs[path] = result
-
-    def block_unpickle(self, path):
-        return self.blobs[path]
-
-    def gsutil_cp(self, src_url, dst_url):
-        """
-        Emulate gsutil with the cp command.
-        """
-        if src_url.startswith("gs://"):
-            assert dst_url.startswith("/")
-            if src_url in self.blobs:
-                assert not dst_url.endswith("/")
-                open(dst_url, "wb").write(self.blobs[src_url])
-            else:
-                # When copying from a bucket directory to a file path, the
-                # directory is copied over. For example, the following:
-                #
-                #   gsutil cp -r gs://my-bucket/data dir
-                #
-                # results in files with names like dir/data/a/b/c.
-
-                last_component = src_url.rsplit("/", 1)[1]
-                directory = Path(dst_url, last_component)
-                directory.mkdir(parents=True, exist_ok=True)
-                for url, data in self.blobs.items():
-                    if not url.startswith(src_url):
-                        continue
-                    filename = url.rsplit("/", 1)[1]
-                    open(Path(directory, filename), "wb").write(data)
-        else:
-            assert src_url.startswith("/")
-            assert dst_url.startswith("gs://")
-            if os.path.isfile(src_url):
-                data = open(src_url, "rb").read()
-                if dst_url.endswith("/"):
-                    filename = os.path.basename(src_url)
-                    self.blobs[f"{dst_url}{filename}"] = data
-                else:
-                    self.blobs[dst_url] = data
-            else:
-                for filename in glob(f"{src_url}/**"):
-                    data = open(filename, "rb").read()
-                    self.blobs[filename.replace(src_url, dst_url, 1)] = data
 
 
 @contextmanager
