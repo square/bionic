8407622862a0f8b56e04da42d210d9f5d959c6cb
Naman Jain <naman@squareup.com>
2020-10-15 12:29:04 -0400

    Bump version: 0.9.0 → 0.9.1

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = u"2019, Square"
 author = u"Janek Klawe"
 
 # The short X.Y version
-version = u"0.9.0"
+version = u"0.9.1"
 # The full version, including alpha/beta/rc tags
 release = version
 
